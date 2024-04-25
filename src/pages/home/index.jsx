afdf446ed1cd30a0cd5fde95ed0c92c87ef9162a
no215tyler <no.215.tyler@gmail.com>
2024-04-25 22:32:14 +0900

    first commit

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>ジャンケンに挑戦しよう!</h1>
+      </div>
+    );
+  }
+}
