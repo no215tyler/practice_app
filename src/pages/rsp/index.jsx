b491572e58e196fe050043ba9922159547a9bdf8
no215tyler <no.215.tyler@gmail.com>
2024-04-26 01:18:15 +0900

    img配置

--- src/pages/rsp/index.jsx
+++ src/pages/rsp/index.jsx
@@ -5,9 +5,22 @@ export default class Index extends React.Component {
   render() {
     return (
       <div className="flex items-center flex-col">
-        <h1 className="text-3xl font-bold text-center">じゃんけんページ</h1>
+        <h1 className="text-3xl font-bold text-center my-3">じゃんけんページ</h1>
+        <div className="flex my-3">
+          <button>
+            <img src="images/rock.jpeg" alt="rock" className="h-[100px] w-[100px] border-2"></img>
+          </button>
+          <button>
+            <img src="images/paper.jpeg" alt="paper" className="h-[100px] w-[100px] border-2"></img>
+          </button>
+          <button>
+            <img src="images/scissors.jpeg" alt="scissors" className="h-[100px] w-[100px] border-2"></img>
+          </button>
+        </div>
+
         <Link to="/">
-          <h1 className="text-3xl font-bold text-center">じゃんけんを終了する</h1>
+          <h1 className="text-3xl font-bold text-center my-3">じゃんけんを終了する</h1>
+          <h4 className="text-xl mt-4">※戦績が発表されます。</h4>
         </Link>
       </div>
     );
